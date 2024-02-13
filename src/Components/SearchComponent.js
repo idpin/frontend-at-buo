caaa4a42a1280f4d5479d14e30325867ef78e6a6
idpin <pinoignacio@uniovi.es>
2024-02-13 12:13:00 +0100

    error let clickSearch

--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -6,28 +6,33 @@ let SearchComponent = (props) => {
 
     let [issn, setIssn] = useState("");
 
-    let ChangeIssn = (e) => {
+
+    let onChangeIssn = (e) => {
         setIssn(+e.currentTarget.value)
     }
 
     let clickSearch = async () => {
-        let response = await fetch(backendURL+"/search"+issn, {
-            headers: { "Content-Type": "application(json" },
-            params : JSON.stringify({ 
-                issn
+        let response = await fetch(backendURL+"/journals", [issn], {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            params : JSON.stringify({
+                issn: issn
             })
-        })
         
+        })
+      
+    
         if ( response.ok ){
             let jsonData = await response.json();
         }
+      
     }
 
 
     return (
         <div className="search-form">
             <h2>Encuentra una revista participante</h2>
-            <input type="text" className="search-input" placeholder="issn" onChange={ChangeIssn}/>
+            <input type="text" className="search-input" placeholder="issn" onChange={onChangeIssn}/>
             <button onClick={() => {clickSearch(3)}} className="search-button">Search</button>
         </div>
     )
