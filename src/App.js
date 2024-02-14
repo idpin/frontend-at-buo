da183d15e49912f4848f617ad09c4d1f24f8a810
unknown <pinoignacio@uniovi.es>
2024-02-14 19:04:49 +0100

    solución problemas con fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import { Route, Routes } from "react-router-dom"
 import SearchComponent from './Components/SearchComponent';
 
 function App() {
@@ -8,12 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Acuerdos Transformativos Universidad de Oviedo</h1>
-      <Routes>
-        <Route path="/journals" element={
         <SearchComponent />
-        }/>
-      </Routes>
     </div>
   );
 }
--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -4,31 +4,41 @@ import { backendURL } from "../Globals";
 
 let SearchComponent = (props) => {
 
-    let [issn, setIssn] = useState("");
+let [issn,setIssn] = useState("")
 
-    let ChangeIssn = (e) => {
-        setIssn(+e.currentTarget.value)
+let [message, setMessage] = useState("")
+
+    let onChangeIssn = (e) => {
+        setIssn(e.currentTarget.value)
     }
 
     let clickSearch = async () => {
-        let response = await fetch(backendURL+"/search"+issn, {
-            headers: { "Content-Type": "application(json" },
-            params : JSON.stringify({ 
-                issn
+        let response = await fetch(backendURL+"/journals/"+issn, {
+            method: "GET",
+            mode: "cors",
+            headers: { "Content-Type": "application/json" },
+            params: JSON.stringify({
+                issn: issn
             })
         })
-        
+
         if ( response.ok ){
             let jsonData = await response.json();
+            setMessage(JSON.stringify(jsonData))
+        } else {
+            let jsonData = await response.json();
+            setMessage(jsonData.error+": "+issn)
         }
     }
 
-
     return (
         <div className="search-form">
+            
+            <h1>Acuerdos Transformativos UniOvi</h1> 
             <h2>Encuentra una revista participante</h2>
-            <input type="text" className="search-input" placeholder="issn" onChange={ChangeIssn}/>
-            <button onClick={() => {clickSearch(3)}} className="search-button">Search</button>
+            <input type="text" className="search-input" placeholder="issn" onChange={onChangeIssn}/>
+            <button onClick={() => {clickSearch()}} className="search-button">Buscar</button>
+            <div>{message}</div>
         </div>
     )
 }
